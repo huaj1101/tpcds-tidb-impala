53d1392a688bba4e495cd5344c87d28f734c9d71
huaj <huaj@mctech.vip>
2022-03-22 03:08:07 +0800

    增加tidb sql

--- scripts/tidb.sql
+++ scripts/tidb.sql
@@ -1,10 +1,10 @@
 --
 -- adjust the schema name if necessary
--- currently (tpcds_50_kudu)
+-- currently (tpcds_50_parquet)
 --
 
-create schema if not exists tpcds_50_kudu;
-use tpcds_50_kudu;
+create schema if not exists tpcds_50_parquet;
+use tpcds_50_parquet;
 
 --
 -- unpartitioned tables
@@ -380,10 +380,9 @@ create table inventory
   inv_item_sk                 integer,
   inv_warehouse_sk            integer,
   inv_quantity_on_hand        integer,
-  primary key (inv_item_sk, inv_warehouse_sk)
+  PRIMARY KEY (inv_item_sk, inv_warehouse_sk) /*T![clustered_index] CLUSTERED */
 )
-partition by hash (inv_item_sk) partitions 12
-stored as kudu;
+;
 
 create table store_sales
 (
@@ -409,10 +408,9 @@ create table store_sales
   ss_net_paid                 decimal(7,2),
   ss_net_paid_inc_tax         decimal(7,2),
   ss_net_profit               decimal(7,2),
-  primary key (ss_sold_time_sk, ss_item_sk, ss_customer_sk, ss_cdemo_sk, ss_hdemo_sk, ss_addr_sk, ss_store_sk, ss_promo_sk)
+  PRIMARY KEY (ss_sold_time_sk, ss_item_sk, ss_customer_sk, ss_cdemo_sk, ss_hdemo_sk, ss_addr_sk, ss_store_sk, ss_promo_sk) /*T![clustered_index] CLUSTERED */
 )
-partition by hash (ss_sold_time_sk) partitions 28
-stored as kudu;
+;
 
 create table store_returns
 (
@@ -435,10 +433,9 @@ create table store_returns
   sr_reversed_charge        decimal(7,2),
   sr_store_credit           decimal(7,2),
   sr_net_loss               decimal(7,2),
-  primary key (sr_return_time_sk, sr_item_sk, sr_customer_sk, sr_cdemo_sk, sr_hdemo_sk, sr_addr_sk, sr_store_sk, sr_reason_sk)
+  PRIMARY KEY (sr_return_time_sk, sr_item_sk, sr_customer_sk, sr_cdemo_sk, sr_hdemo_sk, sr_addr_sk, sr_store_sk, sr_reason_sk) /*T![clustered_index] CLUSTERED */
 )
-partition by hash (sr_return_time_sk) partitions 3
-stored as kudu;
+;
 
 create table catalog_returns
 (
@@ -468,10 +465,9 @@ create table catalog_returns
   cr_reversed_charge        decimal(7,2),
   cr_store_credit           decimal(7,2),
   cr_net_loss               decimal(7,2),
-  primary key (cr_returned_time_sk,cr_item_sk,cr_refunded_customer_sk,cr_refunded_cdemo_sk,cr_refunded_hdemo_sk,cr_refunded_addr_sk,cr_returning_customer_sk,cr_returning_cdemo_sk,cr_returning_hdemo_sk,cr_returning_addr_sk,cr_call_center_sk,cr_catalog_page_sk,cr_ship_mode_sk,cr_warehouse_sk,cr_reason_sk)
+  PRIMARY KEY (cr_returned_time_sk, cr_item_sk, cr_refunded_customer_sk, cr_refunded_cdemo_sk, cr_refunded_hdemo_sk, cr_refunded_addr_sk, cr_returning_customer_sk, cr_returning_cdemo_sk, cr_returning_hdemo_sk, cr_returning_addr_sk, cr_call_center_sk, cr_catalog_page_sk, cr_ship_mode_sk, cr_warehouse_sk, cr_reason_sk) /*T![clustered_index] CLUSTERED */
 )
-partition by hash (cr_returned_time_sk) partitions 2
-stored as kudu;
+;
 
 create table catalog_sales
 (
@@ -508,10 +504,9 @@ create table catalog_sales
   cs_net_paid_inc_ship      decimal(7,2),
   cs_net_paid_inc_ship_tax  decimal(7,2),
   cs_net_profit             decimal(7,2),
-  primary key (cs_sold_time_sk)
+  PRIMARY KEY (cs_sold_time_sk,cs_ship_date_sk,cs_bill_customer_sk,cs_bill_cdemo_sk,cs_bill_hdemo_sk,cs_bill_addr_sk,cs_ship_customer_sk,cs_ship_cdemo_sk,cs_ship_hdemo_sk,cs_ship_addr_sk,cs_call_center_sk,cs_catalog_page_sk,cs_ship_mode_sk,cs_warehouse_sk,cs_item_sk,cs_promo_sk) /*T![clustered_index] CLUSTERED */
 )
-partition by hash (cs_sold_time_sk) partitions 2
-stored as kudu;
+;
 
 create table web_returns
 (
@@ -537,10 +532,10 @@ create table web_returns
   wr_refunded_cash          decimal(7,2),
   wr_reversed_charge        decimal(7,2),
   wr_account_credit         decimal(7,2),
-  wr_net_loss               decimal(7,2)
+  wr_net_loss               decimal(7,2),
+  PRIMARY KEY (wr_returned_time_sk,wr_item_sk,wr_refunded_customer_sk,wr_refunded_cdemo_sk,wr_refunded_hdemo_sk,wr_refunded_addr_sk,wr_returning_customer_sk,wr_returning_cdemo_sk,wr_returning_hdemo_sk,wr_returning_addr_sk,wr_web_page_sk,wr_reason_sk) /*T![clustered_index] CLUSTERED */
 )
-partitioned by (wr_returned_date_sk integer)
-stored as kudu;
+;
 
 create table web_sales
 (
@@ -576,7 +571,7 @@ create table web_sales
   ws_net_paid_inc_tax       decimal(7,2),
   ws_net_paid_inc_ship      decimal(7,2),
   ws_net_paid_inc_ship_tax  decimal(7,2),
-  ws_net_profit             decimal(7,2)
+  ws_net_profit             decimal(7,2),
+  PRIMARY KEY (ws_sold_time_sk,ws_ship_date_sk,ws_item_sk,ws_bill_customer_sk,ws_bill_cdemo_sk,ws_bill_hdemo_sk,ws_bill_addr_sk,ws_ship_customer_sk,ws_ship_cdemo_sk,ws_ship_hdemo_sk,ws_ship_addr_sk,ws_web_page_sk,ws_web_site_sk,ws_ship_mode_sk,ws_warehouse_sk,ws_promo_sk) /*T![clustered_index] CLUSTERED */
 )
-partitioned by (ws_sold_date_sk integer)
-stored as kudu;
+;
